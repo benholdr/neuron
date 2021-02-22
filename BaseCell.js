311c8fbdb944fc86028d3a06fcbeecde6612f12a
benholdr <benholdr@gmail.com>
2021-02-21 21:16:44 -0600

    base neuron
    only works in babel I will need to refactor to get it to work as is.

---
+++ BaseCell.js
@@ -0,0 +1,32 @@
+class{
+  #inputWeights;
+  #output = true;
+  #outputDecay;
+  
+  //expects a function of weights
+  constructor(mutate){
+    this.inputWeights = mutate();
+  }
+  
+  //accepts array of values for calulation 
+  set checkInput(input){
+    #output = this.#inputWeights(input);
+  }
+  
+  //returns if the neuron is in a state of fire 
+  get ouput (){
+    result = this.#output;
+    this.#output = outputDecay();
+    return result;
+  }
+  
+  //updates function to calculate output
+  updateInputWeights(mutate){
+    this.#inputWeights = mutate
+  }
+  
+  //updates the function to decay the output
+  updateOutputDecay(mutate){
+    this.#outputDecay = mutate();
+  }
+}
