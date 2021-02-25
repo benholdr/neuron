970f43e0e1c785e758f5c66f485e3f57ea8aebbc
benholdr <benholdr@gmail.com>
2021-02-24 19:11:03 -0600

    Update BaseCell.js
    updating and testing

--- BaseCell.js
+++ BaseCell.js
@@ -1,33 +1,41 @@
-class{
-  #inputWeights;
-  #output = true;
-  #outputDecay;
-  #decayCounter = 0;
+class neuron{
   
   //expects a function of weights
-  constructor(mutate){
-    this.inputWeights = mutate();
+  constructor(){
+    this.inputWeights = null;
+    this.output = true;
+    this.outputDecay = null;
+    this.decayCounter = 0;
   }
   
   //accepts array of values for calulation 
-  set checkInput(input){
-    #output = this.#inputWeights(input);
+  checkInput(input){
+    this.output = this.inputWeights;
   }
   
   //returns if the neuron is in a state of fire 
-  get ouput (){
-    result = this.#output;
-    this.#output = outputDecay();
+  ouputVal(){
+    let result = this.output;
+    this.output = this.outputDecay;
     return result;
   }
   
   //updates function to calculate output
   updateInputWeights(mutate){
-    this.#inputWeights = mutate
+    this.inputWeights = mutate;
   }
   
   //updates the function to decay the output
   updateOutputDecay(mutate){
-    this.#outputDecay = mutate();
+    this.outputDecay = mutate;
+    console.log("here");
   }
 }
+
+let thing = new neuron();
+thing.updateInputWeights("some value");
+thing.updateOutputDecay("some other value");
+
+console.log(thing.checkInput());
+console.log(thing.ouputVal());
+console.log(thing.outoutDecay);
